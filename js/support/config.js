857e3df779def415f212253a87cde61f041a4cac
sawi-kpc <31955957+sawi-kpc@users.noreply.github.com>
2026-04-16 13:48:10 +0700

    Update config.js

--- js/support/config.js
+++ js/support/config.js
@@ -5,7 +5,7 @@
 ══════════════════════════════════════════════════════════════ */
 
 var SUPPORT_APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwfxYfSy9zdtX8XxPgoeBUtkrDhPoGak7WsYp6zp8HTbIXHro5JBKWNROc2Lt7o4tTb/exec';
-var SUPPORT_JIRA_BASE       = 'https://kingpower.atlassian.net/browse/';
+var SUPPORT_JIRA_BASE       = 'https://kingpowerclick.atlassian.net/browse/';
 
 var SUPPORT_CACHE_CONFIG = {
   ttlMinutes: 60,
